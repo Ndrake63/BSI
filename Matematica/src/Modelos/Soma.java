0c2e3370960584da73c45dc9afa04ed00e9e74c4
Ndrake63 <139346881+Ndrake63@users.noreply.github.com>
2023-08-24 22:29:13 -0300

    Calculadora básica
    Calculadora básica com multi telas

---
+++ Matematica/src/Modelos/Soma.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author alunos
+ */
+public class Soma {
+    float nr1;
+    float nr2;
+
+    public float getNr1() {
+        return nr1;
+    }
+
+    public void setNr1(float nr1) {
+        this.nr1 = nr1;
+    }
+
+    public float getNr2() {
+        return nr2;
+    }
+
+    public void setNr2(float nr2) {
+        this.nr2 = nr2;
+    }
+}
